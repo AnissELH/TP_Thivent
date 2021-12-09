de2066673e9d1f5049baf06b8243803ebcbf5270
AnissELH <56992616+AnissELH@users.noreply.github.com>
2021-12-10 00:05:35 +0100

    Add files via upload

---
+++ TP ELHACHIMI et SABIRI/cmake-build-debug/CMakeFiles/TP_Thivent.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_Thivent.dir/fenetre.cpp.obj"
+  "CMakeFiles/TP_Thivent.dir/main.cpp.obj"
+  "TP_Thivent.exe"
+  "TP_Thivent.exe.manifest"
+  "TP_Thivent.pdb"
+  "libTP_Thivent.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_Thivent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
